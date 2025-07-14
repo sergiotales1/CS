d1f009f83790d9001958286c1e117af1f778353a
SergioTales1 <algumemailai3@gmail.com>
2025-07-13 22:41:27 -0300

    leetcode review

--- leet-code/longest_common_prefix.py
+++ leet-code/longest_common_prefix.py
@@ -1,7 +1,51 @@
 def longestCommonPrefix(strs):
-  for word in strs:
-    for letter in word:
-      print(letter)
+  sortedStrs = sorted(strs) # sort alphabetically
+  first = sortedStrs[0] # get first item
+  last = sortedStrs[sortedStrs.__len__() - 1] # get last item
+  result = "" # initialize result
+  for i, n in enumerate(first):
+    if first[i] != last[i]:
+      break
+    result = result + first[i]
 
-strs = ["oi", "hi"]
-longestCommonPrefix(strs)
+  return result
+
+
+def longestCommonPrefixImproved(strs):
+  pref = strs[0]
+  pref_len = len(pref)
+
+  for s in strs[1:]:
+    while pref != s[0:pref_len]:
+      pref_len -= 1
+      if pref_len == 0:
+        return ""
+      pref = pref[0: pref_len]
+  return pref
+
+def mv(strs):
+  pref = strs[0]
+  pref_len = len(pref)
+
+  for s in strs[1:]:
+    while pref != s[0:pref_len]:
+      pref_len -= 1
+      if pref_len == 0:
+        return ""
+      pref = s[0:pref_len]
+  return pref
+
+
+
+
+strs = ["hey", "he", "hello"]
+strs = ["food", "fool", "family"]
+print(mv(strs))
+# print(longestCommonPrefixImproved(strs))
+
+
+
+
+sortedArray = longestCommonPrefix(strs)
+
+# print(sortedArray)
--- leet-code/two_sum.py
+++ leet-code/two_sum.py
@@ -12,7 +12,32 @@ def twoSum(nums: List[int], target: int) -> List[int]: # List is just a type we 
     return
 
 
+def mv(arr, target):
+    prevMap = {} # index ; value
+
+    for i, n in enumerate(arr):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+
 nums = [2, 7, 4, 11, 15]
 target = 15
-result = twoSum(nums, target)
+# result = twoSum(nums, target)
+result = mv(nums, target)
 print(f"Result: {result}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
